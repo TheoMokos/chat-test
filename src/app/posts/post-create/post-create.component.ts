8f1bd44949d409c22c56aa1a8bc64b33b9f2d1a6
TheoMokos <theo.mokos@gmail.com>
2023-02-05 21:13:21 +0200

    This is a commit after the change of add and get post as well as the implementaiton of observable

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-create',
@@ -6,16 +9,17 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent {
-  enteredTitle = '';
   enteredContent = '';
+  enteredTitle = '';
 
-  @Output() postCreated = new EventEmitter();
+  constructor(public postService: PostsService) {
 
-  onAddPost() {
-    const post = {
-      title: this.enteredTitle,
-      content: this.enteredContent
-    };
-    this.postCreated.emit(post);
+  }
+
+  onAddPost(form: NgForm) {
+    if (form.invalid){
+      return;
+    }
+    this.postService.addPosts(form.value.title, form.value.content);
   }
 }
